526bb59eeea33ae88f4e741f4ad3a120dd8dc094
Ido Kendler <idokn@mta.ac.il>
2020-09-05 09:58:54 +0300

    5.9 added fx ui and main window xml

--- FXUI/src/UI.java
+++ FXUI/src/UI.java
@@ -1,41 +1,21 @@
-import SDMCommon.*;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-import java.net.URL;
-
 public class UI extends Application {
-
-    public Stage s;
-
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-
-        FXMLLoader loader = new FXMLLoader();
-        URL mainFXML = getClass().getResource("MainWindow.fxml");
-        loader.setLocation(mainFXML);
-
-        ScrollPane root = loader.load();
-
-        mainWindowController controller = loader.getController();
-        MarketArea SDM = new MarketArea();
-        controller.setPrimaryStage(primaryStage);
-        controller.setSDM(SDM);
-        controller.setStyle("Lime.css");
         primaryStage.setTitle("Super Duper Market");
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("Lime.css");
+        Parent load = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Scene scene = new Scene(load, 600,400);
+
         primaryStage.setScene(scene);
-
-         s=primaryStage;
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
--- SuperDuperMarket/ConsoleUI/src/Main.java
+++ SuperDuperMarket/ConsoleUI/src/Main.java
@@ -9,10 +9,10 @@ import java.io.InputStream;
 public class Main {
 
 
-  // public static void main(String[] args) {
-  //     ConsoleUI ui = new ConsoleUI();
-  //     ui.run();
-  // }
+    //public static void main(String[] args) {
+    //    ConsoleUI ui = new ConsoleUI();
+    //    ui.run();
+    //}
 
 
 }
