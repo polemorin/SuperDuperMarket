488b257c6b173f9d717b37860da62120427aac6b
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-07 14:22:44 +0300

    added Place order fxml (many)

---
+++ FXUI/src/ProductTileController.java
@@ -0,0 +1,37 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ProductTileController {
+
+    @FXML
+    private Label NameLabel;
+
+    @FXML
+    private Button PlusButton;
+
+    @FXML
+    private Button MinusButton;
+
+    @FXML
+    private Label SoldByLabel;
+
+    @FXML
+    private Label AmountLabel;
+
+    @FXML
+    private Label PriceLabel;
+
+    @FXML
+    void MinusButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PlusButtonAction(ActionEvent event) {
+
+    }
+
+}
