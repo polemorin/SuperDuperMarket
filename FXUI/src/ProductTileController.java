a70ab14d77e75dae70e398f7a143fc1b99581b08
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-07 17:57:04 +0300

    finish working on first screen of place order. finish the products tile controller

--- FXUI/src/ProductTileController.java
+++ FXUI/src/ProductTileController.java
@@ -1,4 +1,7 @@
 
+import ProductTypes.Product;
+import ProductTypes.ProductCategory;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -6,8 +9,17 @@ import javafx.scene.control.Label;
 
 public class ProductTileController {
 
+    private Product product = null;
+    private Double price = null;
+    private String category = null;
+    private Double changeAmountByButtons = null;
+    private SimpleDoubleProperty amountLabelProperty = new SimpleDoubleProperty(0.0);
+
     @FXML
     private Label NameLabel;
+
+    @FXML
+    private Label PriceStaticLabel;
 
     @FXML
     private Button PlusButton;
@@ -24,14 +36,38 @@ public class ProductTileController {
     @FXML
     private Label PriceLabel;
 
+
+    public void setData(Product product, Double price){
+        this.product = product;
+        this.category = product.getProductCategory().toString().equals("Quantity") ? "Units" : "Kilo";
+        this.price = price;
+        changeAmountByButtons = category.equals("Units") ? 1 : 0.1;
+        NameLabel.setText(product.getProductName());
+        SoldByLabel.setText(category);
+        if(price == null){
+            PriceLabel.visibleProperty().setValue(false);
+            PriceStaticLabel.visibleProperty().setValue(false);
+        }
+        else{
+            PriceLabel.setText(String.format("%.2f",price));
+        }
+
+    }
+    @FXML
+    void initialize(){
+        AmountLabel.textProperty().bind(amountLabelProperty.asString());
+    }
+
     @FXML
     void MinusButtonAction(ActionEvent event) {
-
+        if(amountLabelProperty.get() > 0.0){
+            amountLabelProperty.set(amountLabelProperty.getValue() + changeAmountByButtons);
+        }
     }
 
     @FXML
     void PlusButtonAction(ActionEvent event) {
-
+        amountLabelProperty.set(amountLabelProperty.getValue() + changeAmountByButtons);
     }
 
 }
