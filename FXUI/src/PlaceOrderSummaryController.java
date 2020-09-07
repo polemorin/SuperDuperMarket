2fc7002188654d4a9c93f45f30c55e517b94884d
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-07 17:57:04 +0300

    finish working on first screen of place order. finish the products tile controller

---
+++ FXUI/src/PlaceOrderSummaryController.java
@@ -0,0 +1,38 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class PlaceOrderSummaryController {
+
+    @FXML
+    private VBox StoreLevelOrderVbox;
+
+    @FXML
+    private Label ProductsPriceLabel;
+
+    @FXML
+    private Label DeliveryPriceLabel;
+
+    @FXML
+    private Label TotalPriceLabel;
+
+    @FXML
+    private Button CancelOrderButton;
+
+    @FXML
+    private Button ConfirmOrderButton;
+
+    @FXML
+    void CancelOrderButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ConfirmOrderButtonAction(ActionEvent event) {
+
+    }
+
+}
