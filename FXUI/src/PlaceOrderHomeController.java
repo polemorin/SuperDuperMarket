0984b7b2fd7652053886864fc2c1a6717e16b4db
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-07 14:22:44 +0300

    added Place order fxml (many)

---
+++ FXUI/src/PlaceOrderHomeController.java
@@ -0,0 +1,62 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class PlaceOrderHomeController {
+
+    @FXML
+    private ComboBox<?> CustomerComboBox;
+
+    @FXML
+    private DatePicker DeliveryDatePicker;
+
+    @FXML
+    private RadioButton StaticRadio;
+
+    @FXML
+    private RadioButton DynamicRadio;
+
+    @FXML
+    private ComboBox<?> StoreComboBox;
+
+    @FXML
+    private Label DeliveryPriceLabel;
+
+    @FXML
+    private Button NextButton;
+
+    @FXML
+    void CustomerComboBoxAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeliveryDatePickerAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DynamicRadioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void NextButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StaticRadioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StoreComboBoxAction(ActionEvent event) {
+
+    }
+
+}
---
+++ FXUI/src/PlaceOrderProductsController.java
@@ -0,0 +1,20 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+
+public class PlaceOrderProductsController {
+
+    @FXML
+    private FlowPane ProductsFlowPane;
+
+    @FXML
+    private Button ContinueButton;
+
+    @FXML
+    void ContinueButtonAction(ActionEvent event) {
+
+    }
+
+}
