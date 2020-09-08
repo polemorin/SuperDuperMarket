5c501575a13abc95da7089ab4711163e05511348
Ido Kendler <idokn@mta.ac.il>
2020-09-08 09:07:33 +0300

    Test

--- FXUI/src/PlaceOrderProductsController.java
+++ FXUI/src/PlaceOrderProductsController.java
@@ -1,11 +1,23 @@
 
+import ProductTypes.Product;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Map;
 
 public class PlaceOrderProductsController {
 
@@ -14,8 +26,9 @@ public class PlaceOrderProductsController {
     private User customer;
     private Store store;
     private LocalDate deliveryDate;
-
-
+    Stage placeOrderSalesStage;
+    @FXML
+    private ScrollPane ScrollProductPane;
     @FXML
     private FlowPane ProductsFlowPane;
 
@@ -24,21 +37,62 @@ public class PlaceOrderProductsController {
 
     @FXML
     void ContinueButtonAction(ActionEvent event) {
-
+        if(placeOrderSalesStage == null){
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("PlaceOrderSales.fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+                placeOrderSalesStage = new Stage();
+                placeOrderSalesStage.setTitle("Place Order Sales");
+                placeOrderSalesStage.setScene(scene);
+                placeOrderSalesStage.setAlwaysOnTop(true);
+                //placeOrderProductsStage.initOwner(primaryStage);
+                placeOrderSalesStage.initModality(Modality.WINDOW_MODAL);
+                PlaceOrderSalesController placeOrderSalesController = fxmlLoader.getController();
+               // placeOrderSalesController.setData(SDM,customer,store,date);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Stage s = (Stage)(ContinueButton.getScene().getWindow());
+        s.close();
+        //onClose();
+        placeOrderSalesStage.showAndWait();
     }
 
-    public void setData(SuperDuperMarket sdm, User customer, Store store, LocalDate date) {
+    public void setData(SuperDuperMarket sdm, User customer, Store store, LocalDate date) throws IOException {
         SDM = sdm;
         this.customer = customer;
         this.store = store;
         deliveryDate = date;
+        //THIS CODE MAKES THE FLOW PANE FILL ALL THE WIDTH OF HIS ROOT PANE(SCRO LLPANE)
+ //       ScrollProductPane.viewportBoundsProperty().addListener(new ChangeListener<Bounds>() {
+ // @Override public void changed(ObservableValue<? extends Bounds> bounds, Bounds oldBounds, Bounds newBounds) {
+ //               ProductsFlowPane.setPrefWidth(newBounds.getWidth());
+ //           }
+ //       });
+ //       //
+    //   setProducts();
     }
 
 
-    private void setProducts(){
-
-        if(store == null){
-
+    private void setProducts() throws IOException {
+        try {
+            FXMLLoader fxmlLoader;
+            Node ProductTile;
+            ProductTileController productTileController;
+            if (store == null) {
+                for (Map.Entry<Integer, Product> product : SDM.getProducts().entrySet()) {
+                    fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("ProductTile.fxml"));
+                    ProductTile = fxmlLoader.load();
+                    productTileController = fxmlLoader.getController();
+                    productTileController.setData(product.getValue(), null);
+                    ProductsFlowPane.getChildren().add(ProductTile);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
