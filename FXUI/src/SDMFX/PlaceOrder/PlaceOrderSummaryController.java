6642ba6fa664fe53a2ef3c232023c549ea08ecce
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-14 15:33:07 +0300

    files

---
+++ FXUI/src/SDMFX/PlaceOrder/PlaceOrderSummaryController.java
@@ -0,0 +1,91 @@
+package SDMFX.PlaceOrder;
+import SDMCommon.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlaceOrderSummaryController {
+
+    SuperDuperMarket SDM;
+    CustomerLevelOrder order;
+    @FXML
+    private VBox StoreLevelOrderVbox;
+
+    @FXML
+    private Label ProductsPriceLabel;
+
+    @FXML
+    private Label DeliveryPriceLabel;
+
+    @FXML
+    private Label TotalPriceLabel;
+
+    @FXML
+    private Button CancelOrderButton;
+
+    @FXML
+    private Button ConfirmOrderButton;
+    @FXML
+    private Label DeliveryDateLabel;
+
+    @FXML
+    private Label CustomerNameLabel;
+
+    @FXML
+    private Label CustomerIDLabel;
+
+    @FXML
+    void CancelOrderButtonAction(ActionEvent event) {
+        Stage s = (Stage)CancelOrderButton.getScene().getWindow();
+        s.close();
+    }
+
+    @FXML
+    void ConfirmOrderButtonAction(ActionEvent event) {
+        SDM.placeOrderInSDM(order,order.getOrders().get(0).getCustomerID());
+        Stage s = (Stage)CancelOrderButton.getScene().getWindow();
+        s.close();
+    }
+
+    public void setData(SuperDuperMarket sdm, CustomerLevelOrder customerLevelOrder) {
+        SDM = sdm;
+        order = customerLevelOrder;
+        initStoreLevelOrders();
+        initLabels();
+    }
+
+    private void initLabels() {
+        DeliveryPriceLabel.setText(String.format("%.2f",order.getDeliveryPrice()));
+        ProductsPriceLabel.setText(String.format("%.2f",order.getTotalProductPrice()));
+        TotalPriceLabel.setText(String.format("%.2f",order.getDeliveryPrice()+order.getTotalProductPrice()));
+        DeliveryDateLabel.setText(order.getDate().toString());
+        CustomerIDLabel.setText(Integer.toString(order.getOrders().get(0).getCustomerID()));
+        CustomerNameLabel.setText(SDM.getUsers().get(order.getOrders().get(0).getCustomerID()).getName());
+    }
+
+    public void initStoreLevelOrders(){
+        FXMLLoader fxmlLoader;
+        Node storeOrderTile;
+        StoreLevelOrderController storeLevelOrderController;
+        for (StoreLevelOrder storeLevelOrder:order.getOrders()) {
+            try{
+                fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("SDMFX/PlaceOrder/StoreLevelOrder.fxml"));
+                storeOrderTile = fxmlLoader.load();
+                storeLevelOrderController = fxmlLoader.getController();
+                storeLevelOrderController.setData(storeLevelOrder,SDM);
+                StoreLevelOrderVbox.getChildren().add(storeOrderTile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ FXUI/src/SDMFX/PlaceOrder/StoreLevelOrderController.java
@@ -0,0 +1,90 @@
+package SDMFX.PlaceOrder;
+import SDMCommon.*;
+import ProductTypes.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreLevelOrderController  {
+    List<SoldProduct> productBought;
+    @FXML
+    private Label NameLabel;
+
+    @FXML
+    private Label IDLabel;
+
+    @FXML
+    private Label PPKLabel;
+
+    @FXML
+    private Label DistanceLabel;
+
+    @FXML
+    private Label DeliveryPriceLabel;
+
+    @FXML
+    private TableView<SoldProduct> ProductsTableView;
+
+    @FXML
+    private TableColumn<SoldProduct, String> NameColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, Integer> IDColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, String> SoldByColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, Double> AmountColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, Double> PriceColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, Double> TotalColumn;
+
+    @FXML
+    private TableColumn<SoldProduct, String> PurchasedOnSaleColumn;
+
+
+    @FXML
+    private void initialize(){
+        NameColumn.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        IDColumn.setCellValueFactory(new PropertyValueFactory<>("productID"));
+        SoldByColumn.setCellValueFactory(new PropertyValueFactory<SoldProduct,String>("categoryString"));
+        AmountColumn.setCellValueFactory(new PropertyValueFactory<>("amountSoldInOrder"));
+        PriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        TotalColumn.setCellValueFactory(new PropertyValueFactory<>("totalPrice"));
+        PurchasedOnSaleColumn.setCellValueFactory(new PropertyValueFactory<>("saleName"));
+    }
+    public void setData(StoreLevelOrder storeLevelOrder, SuperDuperMarket SDM) {
+        productBought = new ArrayList<>();
+        productBought.addAll(storeLevelOrder.getSoldProducts());
+        if(storeLevelOrder.getProductSoldOnSale() != null) {
+            productBought.addAll(storeLevelOrder.getProductSoldOnSale());
+        }
+        final ObservableList<SoldProduct> dataOfItems = FXCollections.observableList(productBought);
+        ProductsTableView.setItems(dataOfItems);
+        ProductsTableView.refresh();
+
+        Point customerLocation = SDM.getUsers().get(storeLevelOrder.getCustomerID()).getLocation();
+        Point storeLocation = SDM.getStores().get(storeLevelOrder.getStoreID()).getLocation();
+        String storeName = SDM.getStores().get(storeLevelOrder.getStoreID()).getName();
+        double ppk = SDM.getStores().get(storeLevelOrder.getStoreID()).getDeliveryPPK();
+
+        DeliveryPriceLabel.setText(String.format("%.2f", storeLevelOrder.getDeliveryPrice()));
+        DistanceLabel.setText(String.format("%.2f",storeLevelOrder.getDistanceFromCustomerToStore(storeLocation,customerLocation)));
+        IDLabel.setText(Integer.toString(storeLevelOrder.getStoreID()));
+        NameLabel.setText(storeName);
+        PPKLabel.setText(Double.toString(ppk));
+    }
+
+}
