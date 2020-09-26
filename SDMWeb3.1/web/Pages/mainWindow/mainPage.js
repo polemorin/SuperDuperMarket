d0a0961fa00984e08fadd0cf8d7f63cf578760ce
Lilach Birnbaum <lilachbi@mta.ac.il>
2020-09-26 14:39:18 +0300

    started working on mainWindow html

---
+++ SDMWeb3.1/web/Pages/mainWindow/mainPage.js
@@ -0,0 +1,30 @@
+
+$(function() { // onload...do
+    //add a function to the submit event
+
+
+
+
+
+
+
+    $("#chatform").submit(function() {
+        $.ajax({
+            data: $(this).serialize(),
+            url: this.action,
+            timeout: 2000,
+            error: function() {
+                console.error("Failed to submit");
+            },
+            success: function(r) {
+                //do not add the user string to the chat area
+                //since it's going to be retrieved from the server
+                //$("#result h1").text(r);
+            }
+        });
+
+        $("#userstring").val("");
+        // by default - we'll always return false so it doesn't redirect the user.
+        return false;
+    });
+});
