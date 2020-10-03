c2f1f3023c81e72cd66669311abc852d5b73fdeb
Ido Kendler <idokn@mta.ac.il>
2020-10-03 13:03:30 +0300

    string to work on page 3

--- SuperDuperMarket/SDMLogic/src/ProductTypes/ProductTableInfo.java
+++ SuperDuperMarket/SDMLogic/src/ProductTypes/ProductTableInfo.java
@@ -1,7 +1,7 @@
 package ProductTypes;
 
 public class ProductTableInfo {
-    int productID;
+    int ID;
     String productName;
     String category;
     int amountOfStoresWhoSellProduct;
@@ -9,7 +9,7 @@ public class ProductTableInfo {
     double amountSoldInMarket;
 
     public ProductTableInfo(int ID, String productName, String category, int amountOfStoresWhoSellProduct, double avgPrice, double amountSoldInMarket) {
-        this.productID = ID;
+        this.ID = ID;
         this.productName = productName;
         this.category = category;
         this.amountOfStoresWhoSellProduct = amountOfStoresWhoSellProduct;
--- SuperDuperMarket/SDMLogic/src/ProductTypes/SoldProduct.java
+++ SuperDuperMarket/SDMLogic/src/ProductTypes/SoldProduct.java
@@ -7,41 +7,21 @@ import java.util.Objects;
 public class SoldProduct extends PricedProduct{
 
     private double amountSoldInOrder;
-    private double totalPrice;
-    private int productID;
-    private String productName;
-    private double price;
-    private String categoryString;
 
     public SoldProduct(int id, String name, ProductCategory cat, double price, int storeID, double amountSoldInOrder, double totalPrice){
         super(id,name,cat,price,storeID);
         this.amountSoldInOrder = amountSoldInOrder;
-        this.totalPrice = totalPrice;
-        this.productID = id;
-        this.productName = name;
-        this.price = price;
-        categoryString = cat.toString();
     }
 
     public SoldProduct(PricedProduct pricedProduct, double amount){
         super(pricedProduct);
         this.amountSoldInOrder = amount;
 
-        this.productID = pricedProduct.getProductID();
-        this.productName = pricedProduct.getProductName();
-        this.price = pricedProduct.getPrice();
-        totalPrice = amountSoldInOrder * this.getPrice();
-        categoryString = pricedProduct.getProductCategory().toString();
     }
 
     public SoldProduct(SoldProduct other){
         super(other);
         this.amountSoldInOrder = other.amountSoldInOrder;
-        this.totalPrice = other.totalPrice;
-        categoryString = other.categoryString;
-    }
-    public String getCategoryString() {
-        return categoryString;
     }
 
     public List<String> getProductStringListToFile(){
@@ -51,7 +31,6 @@ public class SoldProduct extends PricedProduct{
         String price = String.format("%.2f",(Double)(this.getPrice()));
         String storeID = ((Integer)(this.getStoreID())).toString();
         String soldInOrder = ((Double)(amountSoldInOrder)).toString();
-        String totalPriceStr = String.format("%.2f",(Double)(totalPrice));;
 
         productStringList.add(id);
         productStringList.add(this.getProductName());
@@ -59,7 +38,6 @@ public class SoldProduct extends PricedProduct{
         productStringList.add(price);
         productStringList.add(storeID);
         productStringList.add(soldInOrder);
-        productStringList.add(totalPriceStr);
 
         return productStringList;
     }
@@ -89,14 +67,13 @@ public class SoldProduct extends PricedProduct{
     }
     public void setAmountSoldInOrder(double amountSoldInOrder) {
         this.amountSoldInOrder = amountSoldInOrder;
-        totalPrice = amountSoldInOrder*this.getPrice();
     }
     public void addAmountToOrder(double amountToAdd){
         amountSoldInOrder += amountToAdd;
-        totalPrice = amountSoldInOrder*this.getPrice();
+
     }
     public double getTotalPrice() {
-        return totalPrice;
+        return super.getPrice()*amountSoldInOrder;
     }
 
 
@@ -108,7 +85,6 @@ public class SoldProduct extends PricedProduct{
         if (!super.equals(o)) return false;
         SoldProduct that = (SoldProduct) o;
         return Double.compare(that.amountSoldInOrder, amountSoldInOrder) == 0 &&
-                Double.compare(that.totalPrice, totalPrice) == 0 &&
                 this.getProductID() == that.getProductID()&&
                 this.getStoreID() == this.getStoreID();
     }
@@ -117,23 +93,10 @@ public class SoldProduct extends PricedProduct{
     public int hashCode() {
         int res = 17;
         res = 31 * res + (int)amountSoldInOrder;
-        res = 31 * res + (int)totalPrice;
         res = 31 * res + this.getProductID();
         res = 31 * res + this.getStoreID();
         return res;
     }
-    @Override
-    public int getProductID() {
-        return productID;
-    }
 
-    @Override
-    public String getProductName() {
-        return productName;
-    }
 
-    @Override
-    public double getPrice() {
-        return price;
-    }
 }
