c6c16b1b48cc91cacea16bb21638bf4ce4c7821b
Ido Kendler <idokn@mta.ac.il>
2020-10-18 16:27:41 +0300

    chatroom bonus done

--- SDMWeb3.1/src/Utils/ServletUtils.java
+++ SDMWeb3.1/src/Utils/ServletUtils.java
@@ -1,13 +1,20 @@
 package Utils;
 
+import SDMCommon.ChatManager;
 import SDMCommon.SDManager;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import java.awt.*;
+
+import static chat.constants.Constants.INT_PARAMETER_ERROR;
 
 public class ServletUtils {
 
     private static final String SDM_MANAGER_ATTRIBUTE_NAME = "sdmManager";
+    private static final String CHAT_MANAGER_ATTRIBUTE_NAME = "chatManager";
+
+    private static final Object chatManagerLock = new Object();
     private static final Object sdmManagerLock = new Object();
 
     public static SDManager getSDMManager(ServletContext servletContext) {
@@ -18,5 +25,23 @@ public class ServletUtils {
             }
         }
         return (SDManager) servletContext.getAttribute(SDM_MANAGER_ATTRIBUTE_NAME);
+    }
+    public static ChatManager getChatManager(ServletContext servletContext) {
+        synchronized (chatManagerLock) {
+            if (servletContext.getAttribute(CHAT_MANAGER_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(CHAT_MANAGER_ATTRIBUTE_NAME, new ChatManager());
+            }
+        }
+        return (ChatManager) servletContext.getAttribute(CHAT_MANAGER_ATTRIBUTE_NAME);
+    }
+    public static int getIntParameter(HttpServletRequest request, String name) {
+        String value = request.getParameter(name);
+        if (value != null) {
+            try {
+                return Integer.parseInt(value);
+            } catch (NumberFormatException numberFormatException) {
+            }
+        }
+        return INT_PARAMETER_ERROR;
     }
 }
